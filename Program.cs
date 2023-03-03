2c9bcf7e6243cf2d3e23eccbde8b5919ca76353d
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-03-03 20:38:47 -0300

    Exercício frutas preferidas completo feito bagulho ta pronto naqueles pique nao muda nada

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Atividade FrutasPreferidas 03/03/2023 naquele pique
+Console.WriteLine("- Minhas frutas preferidas -");
+Console.WriteLine();
+Console.WriteLine("1. Banana");
+Console.WriteLine("2. Maçã");
+Console.WriteLine("3. Maçã verde");
+Console.WriteLine("4. Morango");
+Console.WriteLine("5. Kiwi");
